baf8f599f564b002ea3280310dd51542105086d7
Shubelle <147102858+Shubelle@users.noreply.github.com>
2025-04-01 19:12:54 -0700

    Create Exercise_1.4.js

---
+++ Week1/Exercise_1.4.js
@@ -0,0 +1,9 @@
+let a = 10
+// Variable a has a value of 10
+
+console.log(a);
+// Prints value of a
+
+/* The purpose of this file is to demonstrate my ability
+to assign a value into a variable and print it using the
+JavaScript language. */
